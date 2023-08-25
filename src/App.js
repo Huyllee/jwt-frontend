0bfebe2a71bdcf237acce0eb5a355f23d6857997
Huyllee <moriarty1807@gmail.com>
2023-08-25 23:13:56 +0700

    loading page when fetch data

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 import "./App.scss";
 import Nav from "./components/Navigation/Nav";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect, useState } from "react";
+import { Oval } from "react-loader-spinner";
 import AppRoutes from "./routes/AppRoutes";
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
 
 function App() {
+  const { user } = useContext(UserContext);
+
   return (
     <>
       <Router>
-        <div className="app-header">
-          <Nav />
-        </div>
-        <div className="App">
-          <AppRoutes />
-
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
-        </div>
+        {user && user.isLoading === true ? (
+          <div className="loading-container">
+            <Oval
+              height={80}
+              width={80}
+              color="#4fa94d"
+              ariaLabel="oval-loading"
+              secondaryColor="#4fa94d"
+              strokeWidth={2}
+              strokeWidthSecondary={2}
+            />
+            <div>Loading data...</div>
+          </div>
+        ) : (
+          <>
+            <div className="app-header">
+              <Nav />
+            </div>
+            <div className="App">
+              <AppRoutes />
+            </div>
+          </>
+        )}
       </Router>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 }
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -5,15 +5,17 @@ const UserContext = React.createContext(null);
 
 const UserProvider = ({ children }) => {
   // User is the name of the "data" that gets stored in context
-  const [user, setUser] = useState({
+  const userDefault = {
+    isLoading: true,
     account: {},
     token: "",
     isAuthenticated: false,
-  });
+  };
+  const [user, setUser] = useState(userDefault);
 
   // Login updates the user data with a name parameter
   const loginContext = (userData) => {
-    setUser(userData);
+    setUser({ ...userData, isLoading: false });
   };
 
   const fetchUser = async () => {
@@ -24,16 +26,24 @@ const UserProvider = ({ children }) => {
       let userName = res.DT.userName;
       let token = res.DT.access_token;
       let data = {
+        isLoading: false,
         isAuthenticated: true,
         token,
         account: { groupWithRole, email, userName },
       };
       setUser(data);
+    } else {
+      setUser({ ...userDefault, isLoading: false });
     }
   };
 
   useEffect(() => {
-    fetchUser();
+    if (
+      window.location.pathname !== "/" ||
+      window.location.pathname !== "/login"
+    ) {
+      fetchUser();
+    }
   }, []);
 
   // Logout updates the user data to default
